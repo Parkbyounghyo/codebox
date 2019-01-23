b63d35550d434da369176a9ba562bea7c3968840
박병효 <redcolor2002@nate.com>
2019-01-23 16:55:08 +0900

    All commit

---
+++ _system/phpStorm-CC-Helpers-master/CodeIgniter3/my_models.php
@@ -0,0 +1,36 @@
+<?php die();
+
+/**
+ * Add you custom models here that you are loading in your controllers
+ *
+ * <code>
+ * $this->site_model->get_records()
+ * </code>
+ * Where site_model is the model Class
+ *
+ * ---------------------- Models to Load ----------------------
+ * <examples>
+ * @property Attendance_model $Attendance_model
+ * @property Autologin_model $Autologin_model
+ * @property Banner_click_log_model $Banner_click_log_model
+ * @property Banner_group_model $Banner_group_model
+ * @property Banner_model $Banner_model
+ * @property Blame_model $Blame_model
+ * @property Board_admin_model $Board_admin_model
+ * @property Board_category_model $Board_category_model
+ * @property Board_group_admin_model $Board_group_admin_model
+ * @property Board_group_meta_model $Board_group_meta_model
+ * @property Board_group_model $Board_group_model
+ * @property Board_meta_model $Board_meta_model
+ * @property Board_model       $Board_model
+ * @property Config_model       $Config_model
+ *
+ *
+ *
+ *
+ */
+class my_models
+{
+}
+
+// End my_models.php
---
+++ application/controllers/Home.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: redco
+ * Date: 2018-10-04
+ * Time: 오후 10:20
+ */
+
+defined('BASEPATH') OR exit('No direct ');
+class Home extends CI_Controller{
+    public function __construct()
+    {
+        parent::__construct();
+//        $this->
+    }
+}
